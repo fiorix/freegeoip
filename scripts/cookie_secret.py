1a69f426ecc5c836e881477768d0ef16e722eb81
fiorix <fiorix@gmail.com>
2013-02-04 15:43:49 -0500

    copyright info

--- scripts/cookie_secret.py
+++ scripts/cookie_secret.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 #
-# Copyright 2013 Alexandre Fiori
-# Powered by cyclone
+# Copyright 2009-2013 Alexandre Fiori
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
