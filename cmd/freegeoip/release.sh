18511d4f436548a08960fdf7b4d7d7d3deda7b95
Alexandre Fiori <fiorix@gmail.com>
2015-11-21 01:53:10 -0500

    Add helper script for binary releases

---
+++ cmd/freegeoip/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$(go run main.go -version | cut -d\  -f2)
+
+function pack() {
+	dir=freegeoip-$VERSION-$1
+	mkdir $dir
+	cp -r freegeoip public ansible-playbook $dir
+	tar czf ${dir}.tar.gz $dir
+	rm -rf $dir
+}
+
+for OS in linux darwin freebsd
+do
+	GOOS=$OS GOARCH=amd64 go build -ldflags '-w'
+	pack $OS-amd64
+done
