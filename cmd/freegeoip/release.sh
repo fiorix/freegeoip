fa59657cc968b2649c4d6d79a4b16ad88ce932ea
Alexandre Fiori <fiorix@gmail.com>
2017-09-30 14:17:41 +0100

    Use gtar on OSX, add support for Windows

--- cmd/freegeoip/release.sh
+++ cmd/freegeoip/release.sh
@@ -5,13 +5,18 @@ VERSION=$(go run main.go -version | cut -d\  -f2)
 function pack() {
 	dir=freegeoip-$VERSION-$1
 	mkdir $dir
-	cp -r freegeoip public $dir
-	tar czf ${dir}.tar.gz $dir
+	cp -r ${binary} public $dir
+	sync
+	gtar --owner=0 --group=0 -czf ${dir}.tar.gz $dir
 	rm -rf $dir
 }
 
-for OS in linux darwin freebsd
+for OS in linux darwin freebsd windows
 do
-	GOOS=$OS GOARCH=amd64 go build -ldflags '-w'
+	binary=freegeoip
+	[ $OS = "windows" ] && binary=${binary}.exe
+	GOOS=$OS GOARCH=amd64 go build -o ${binary} -ldflags '-w -s'
+	sleep 1
 	pack $OS-amd64
+	rm -f ${binary}
 done
